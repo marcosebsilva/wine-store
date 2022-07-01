39b0251fd1b3cc3ae69f0b1ac9ee4ff269515926
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-01 18:09:10 -0300

    feat(fonts): added new fonts and updated global styling

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,10 +1,35 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-* {
-  text-decoration: none;
-  margin: 0;
-  box-sizing: border-box;
-  list-style: none;
-  font-family: 'Lato', sans-serif;
-}`
+  @font-face {
+    font-family: 'NeoSansStd';
+    src: url('/fonts/NeoSans/Neo-Sans-Std-Regular.otf') format('opentype');
+    font-weight: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'NeoSansStd';
+    src: url('/fonts/NeoSans/Neo-Sans-Std-Bold.otf') format('opentype');
+    font-weight: bold;
+    font-display: swap;
+  }
+
+  #__next {
+    height: 100%;
+    overflow: hidden;
+  }
+
+  body {
+    height: 100vh;
+    background-color: #F5F5F5;
+  }
+  * {
+    text-decoration: none;
+    margin: 0;
+    box-sizing: border-box;
+    list-style: none;
+    font-family: 'Lato', sans-serif;
+    padding: 0;
+  }
+`
