e8f33f0486f5a47faa743d80884b28c21ba77a24
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-02 11:11:15 -0300

    feat(context): created CartContext

---
+++ context/CartContext.tsx
@@ -0,0 +1,127 @@
+import React, { useCallback, createContext, useContext, useMemo, useState } from 'react';
+import Product from '../types/Product';
+
+interface ProductInCart extends Product {
+  quantity: number,
+}
+
+interface CartContext {
+  items: ProductInCart[],
+  addToCart: (newItem: Product) => void,
+  removeFromCart: (id: number) => void,
+  increaseItemQuantity: (id: number) => void,
+  decreaseItemQuantity: (id: number) => void,
+}
+
+const initialContext: CartContext = {
+  items: [],
+  removeFromCart: () => {
+    throw new Error('removeFromCart is not implemented')
+  },
+  increaseItemQuantity: () => {
+    throw new Error('increaseItemQuantity is not implemented')
+  },
+  decreaseItemQuantity: () => {
+    throw new Error('decreaseItemQuantity is not implemented')
+  },
+  addToCart: () => {
+    throw new Error('addToCart is not implemented');
+  },
+};
+
+const CartContext = createContext<CartContext>(initialContext);
+
+export function CartProvider({ children }: React.PropsWithChildren) {
+  const [cart, updateCart] = useState<CartContext>(initialContext);
+
+  const findItemInCart = useCallback((id: number) => cart.items.find((item) => item.id === id)
+  , [cart])
+
+  const removeFromCart = useCallback((id: number) => {
+      const item = findItemInCart(id);
+      if (!item) return;
+
+      const itemIndex = cart.items.indexOf(item);
+      const newCartItems = cart.items.splice(itemIndex, 1);
+    
+      updateCart((prev) => ({
+        ...prev,
+        items: newCartItems
+      }));
+  }, [cart, findItemInCart]);
+
+  const decreaseItemQuantity =  useCallback((id: number, amount: number = 1) => {
+    const item = findItemInCart(id);
+    if (!item) return;
+
+    if (amount >= item.quantity) {
+      removeFromCart(item.id)
+      return;
+    }
+  
+    const itemIndex = cart.items.indexOf(item);
+    const newCartItems = [...cart.items];
+
+    newCartItems[itemIndex].quantity -= amount;
+
+    updateCart((prev) => ({
+      ...prev,
+      items: newCartItems
+    }));
+  }, [findItemInCart, removeFromCart, cart]);
+  
+  const increaseItemQuantity = useCallback((id: number, amount: number = 1) => {
+    const item = findItemInCart(id);
+    if (!item) return;
+
+    const itemIndex = cart.items.indexOf(item);
+    const newCartItems = [...cart.items];
+
+    newCartItems[itemIndex].quantity += amount;
+
+    updateCart((prev) => ({
+      ...prev,
+      items: newCartItems
+    }));
+  }, [cart, findItemInCart]);
+
+  const addToCart = useCallback((newItem: Product) => {
+    const item = findItemInCart(newItem.id);
+    if (item) {
+      increaseItemQuantity(newItem.id);
+      return;
+    }
+    const newCartItems = [
+      ...cart.items,
+      {
+        ...newItem,
+        quantity: 1
+      }
+    ];
+
+    updateCart((prev) => ({
+      ...prev,
+      items: newCartItems
+    }));
+  }, [cart, findItemInCart, increaseItemQuantity])
+  
+  const providerValue = useMemo(() => ({
+      ...cart,
+      removeFromCart,
+      increaseItemQuantity,
+      addToCart,
+      decreaseItemQuantity
+    }),
+    [cart, removeFromCart, increaseItemQuantity, addToCart, decreaseItemQuantity],
+  );
+
+  return (
+    <CartContext.Provider value={providerValue}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
