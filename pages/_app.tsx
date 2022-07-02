cd789002d94f474ba851e22e8a552c85fd1610ad
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-02 20:12:01 -0300

    refactor: updated context providers for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app'
 import { Layout } from '../components/Layout';
 import GlobalStyles from '../styles/globals';
 import { SearchProvider } from '../context/SearchContext';
+import { CartProvider } from '../context/CartContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SearchProvider>
-      <GlobalStyles />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CartProvider>
+        <GlobalStyles />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartProvider>
     </SearchProvider>
   )
 }
