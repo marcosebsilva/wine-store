26f7755c3c4d74fafdcf5ed669c6f2c952242aaf
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-03 03:22:35 -0300

    feat: adding new colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,12 +2,14 @@ const colors = {
   light_tanat: "rgba(209, 75, 143, 0.8)",
   gray_darker: "rgba(17, 17, 17, 1)",
   gray_lighter: "rgba(136, 136, 136, 1)",
-  blacker: "rgba(85, 85, 85, 1)",
+  gray: "rgba(85, 85, 85, 1)",
   bold_blacker: "rgba(29, 29, 27, 1)",
   background: "rgba(245, 245, 245, 1)",
   branding_tavel_default: "rgba(247, 149, 82, 1)",
   function_sucess_default: "rgba(126, 188, 67, 1)",
+  function_sucess_dark: "rgba(10, 130, 67, 0.2)",
   wine_purple: "rgba(182, 17, 110, 1)",
+  branding_chenin_default: "rgba(242, 102, 73, 1)",
 }
 
 export default colors;
