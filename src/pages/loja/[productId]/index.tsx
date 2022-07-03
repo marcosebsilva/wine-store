0ef1ec663194d782cdd5ba8ffab1dfa9b978ee72
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-03 03:23:50 -0300

    feat: created ProductPage

--- src/pages/loja/[productId]/index.tsx
+++ src/pages/loja/[productId]/index.tsx
@@ -1,19 +1,128 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import Image from 'next/image';
 import { getAllProducts } from '../../../services/wine-api/calls';
 import { findById } from '../../../services/wine-api/filters';
 import { ParsedUrlQuery } from "querystring";
-import { useEffect } from 'react';
+import RegionDescription from '../../../components/ProductPage/RegionDescription';
 import Product from '../../../types/Product';
 import * as Styled from './style';
+import ProductInfo from '../../../components/ProductPage/ProductInfo';
+import ProductDescription from '../../../components/ProductPage/ProductDescription';
+import PriceContainerMobile from '../../../components/ProductPage/PriceContainerMobile';
+import { useCart } from '../../../context/CartContext';
+import { useEffect, useMemo, useState } from 'react';
+import sizes from '../../../styles/sizes';
+import useScreenSize from '../../../hooks/useScreenSize';
+import PriceContainerDesktop from '../../../components/ProductPage/PriceContainerDesktop';
+import BaseCartButton from '../../../components/UI/BaseCartButton';
+import BaseCounter from '../../../components/UI/BaseCounter';
+import Link from 'next/link';
 
 type ProductDetailsProps = {
   product: Product
 }
 
 const ProductDetails: NextPage<ProductDetailsProps> = ({ product }) => {
+  const {
+    region,
+    country,
+    flag,
+    type,
+    size,
+    image,
+    sommelierComment,
+    discount,
+    price,
+    priceMember,
+    priceNonMember,
+    volume,
+    avaliations = 0,
+    rating,
+    classification
+  } = product;
+
+  const { addToCart } = useCart();
+  const { width } = useScreenSize();
+  const [quantityToIncrease, setQuantity] = useState<number>(1);
+
+  const decreaseQuantity = () => {
+    if(quantityToIncrease <= 1) return;
+    setQuantity((prev) => prev - 1);
+  }
+
+  const isDesktop = useMemo(() => width !== undefined && width >= sizes.desktopSmall, [width]);
+
+  useEffect(() =>{
+    if(!isDesktop){
+      setQuantity(1);
+    }
+  }, [isDesktop])
+
+  const handleButtonClick = () => {
+    addToCart(product, quantityToIncrease);
+  }
+
   return (
     <Styled.Wrapper>
-      <h1>{product.name}</h1>
+      {isDesktop && (
+        <Styled.GoBack>
+          <Styled.Arrow />
+          <Link href="/loja">Voltar</Link>
+        </Styled.GoBack>
+      )
+      }
+      <RegionDescription
+        region={region}
+        country={country}
+      />
+      <Styled.ProductTitle>{product.name}</Styled.ProductTitle>
+      <ProductInfo
+        avaliations={avaliations}
+        classification={classification}
+        rating={rating}
+        country={country}
+        flag={flag}
+        type={type}
+        size={size || volume as string}
+      />
+      <Styled.ImageContainer>
+        <Image 
+          alt="Logotipo principal Wine"
+          src={image}
+          objectFit="contain"
+          layout="fill"
+        />
+      </Styled.ImageContainer>
+      {isDesktop && (
+        <PriceContainerDesktop
+          priceMember={priceMember}
+          priceNonMember={priceNonMember}
+          handleButtonClick={handleButtonClick}
+        />)}
+      <ProductDescription
+        description={sommelierComment}
+      />
+      {!isDesktop && (
+        <PriceContainerMobile
+          discount={discount}
+          price={price}
+          priceMember={priceMember}
+          priceNonMember={priceNonMember}
+          handleButtonClick={handleButtonClick}
+        />
+      )}
+      {isDesktop && (
+        <Styled.DesktopButtonsContainer>
+          <BaseCounter
+            quantity={quantityToIncrease}
+            increaseQuantity={() => setQuantity((prev) => prev + 1)}
+            decreaseQuantity={decreaseQuantity}
+          />
+          <BaseCartButton
+            handleClick={handleButtonClick}
+          />
+        </Styled.DesktopButtonsContainer>
+      )}
     </Styled.Wrapper>
   )
 }
--- src/pages/loja/[productId]/style.ts
+++ src/pages/loja/[productId]/style.ts
@@ -1,4 +1,124 @@
 import styled from "styled-components";
+import devices from "../../../styles/devices";
+import colors from "../../../styles/colors";
+import {Wrapper as BaseCartButtonWrapper} from "../../../components/UI/BaseCartButton/style";
+import {Wrapper as BaseCounterWrapper} from "../../../components/UI/BaseCounter/style";
+import {Wrapper as ProductInfoWrapper} from "../../../components/ProductPage/ProductInfo/style";
+import {Wrapper as RegionDescriptionWrapper} from "../../../components/ProductPage/RegionDescription/style";
+import {Wrapper as PriceContainerDesktopWrapper } from "../../../components/ProductPage/PriceContainerDesktop/style";
+import {Wrapper as NonMemberPriceWrapper} from "../../../components/UI/NonMemberPrice/style";
+import {Wrapper as ProductDescriptionWrapper} from "../../../components/ProductPage/ProductDescription/style";
+import Link from "next/link";
+
+
+export const ProductTitle = styled.h1`
+  text-align: center;
+  font-size: 1.5rem;
+  margin-bottom: 12px;
+`
+
+export const ImageContainer = styled.div`
+  position: relative;
+  text-align: center;
+  height: 333px;
+  width: 218px;
+`
+
+export const DesktopButtonsContainer = styled.div`
+  display: flex;
+  height: 60px;
+  ${BaseCartButtonWrapper} {
+    border: none;
+    border-radius: 0 2px 2px 0;
+    transition: 100ms;
+    &:active {
+      transform: scale(0.99);
+    }
+  }
+
+  ${BaseCounterWrapper}{
+    border-radius: 2px 0 0 2px;
+  }
+`
 
 export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-inline: 20px;
+  @media ${devices.desktopSmall} {
+    display: grid;
+    padding-inline: 15vw;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(7, fit-content);
+    grid-template-areas:
+      "link link"
+      "image region"
+      "image title"
+      "image info"
+      "image price"
+      "image description"
+      "image buttons";
+
+    ${ImageContainer} {
+      grid-area: image;
+      margin: auto;
+      width: 381px;
+      height: 579px;
+    }
+    ${ProductTitle}{
+      grid-area: title;
+      text-align: left;
+      color: ${colors.gray_darker};
+      font-size: 1.75rem;
+      margin-bottom: 0;
+    }
+
+    ${ProductDescriptionWrapper} {
+      grid-area: description;
+    }
+
+    ${ProductInfoWrapper } {
+      justify-content: left;
+      grid-area: info;
+      margin-bottom: 0;
+    }
+
+    ${RegionDescriptionWrapper} {
+      font-weight: bold;
+      font-size: 0.875rem;
+      justify-content: left;
+      grid-area: region;
+    }
+
+    ${PriceContainerDesktopWrapper} {
+      grid-area: price;
+      font-size: 1.5rem;
+    }
+
+    ${DesktopButtonsContainer}{
+      grid-area: buttons;
+      max-width: 300px;
+    }
+
+    ${NonMemberPriceWrapper} {
+      font-size: 1rem;
+      font-weight: bold;
+    }
+  }
+`
+
+export const GoBack = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`
+
+export const Arrow = styled.span`
+  display: block;
+  width: 10px;
+  height: 10px;
+  border-top: 1px solid ${colors.gray_lighter};
+  border-left: 1px solid ${colors.gray_lighter};
+  transform: rotate(-45deg);
 `
