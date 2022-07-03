855b08039e54bbc61c4f06c062f81169afd8511a
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-03 11:32:15 -0300

    style: added animations to BaseCartButton

--- src/components/UI/BaseCartButton/style.ts
+++ src/components/UI/BaseCartButton/style.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import colors from "../../../styles/colors";
+import devices from "../../../styles/devices";
+
 
 export const Wrapper = styled.button`
   width: 100%;
@@ -10,4 +12,14 @@ export const Wrapper = styled.button`
   padding-bottom: 12px;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
+  transition: 200ms;
+
+  @media ${devices.desktopSmall}{
+    &:hover {
+      transform: scale(1.03);
+    }
+    &:active {
+      transform: scale(1);
+    }
+  }
 `
