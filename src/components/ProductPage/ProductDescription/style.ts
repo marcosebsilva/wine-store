6d8eb0291f070ec133c1d831d238958cf3005969
Marcos Estevao <dusdoom.dev@gmail.com>
2022-07-03 03:25:54 -0300

    refactor: updated ProductDescription styling

--- src/components/ProductPage/ProductDescription/style.ts
+++ src/components/ProductPage/ProductDescription/style.ts
@@ -6,11 +6,9 @@ export const Wrapper = styled.div`
   flex-direction: column;
   gap: 15px;
 `
-
 export const Title = styled.h1`
   font-size: 1.25rem;
 `
-
 export const Description = styled.p`
   color: ${colors.gray_lighter};
   line-height: 1.5rem;
